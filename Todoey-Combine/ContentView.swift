9103af42429b07f2e23dfdcb1f5be8c7d3073f27
cp-amisha-i <amisha.i@canopas.com>
2021-06-17 17:17:16 +0530

    Initial Commit

---
+++ Todoey-Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todoey-Combine
+//
+//  Created by Amisha I on 17/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todoey-Combine/Todoey_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todoey_CombineApp.swift
+//  Todoey-Combine
+//
+//  Created by Amisha I on 17/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Todoey_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
